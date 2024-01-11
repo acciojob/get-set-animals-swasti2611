6ac73418e97841ac04580e5d502a56661b84e626
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-11 11:53:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-//complete this code
 class Animal {
-  constructor(species) {
-		this.species=species;
+    constructor(species) {
+          this.species=species;
+    }
+      get getSpecies(){
+          return this.species;
+    }
+    makeSound() {
+          console.log(`The ${this.species} makes a sound`);
+    }
   }
-	get getSpecies(){
-		return this.species;
+  
+  class Dog extends Animal {
+    bark() {
+      console.log("woof");
+    }
   }
-  makeSound() {
-		console.log(`The ${species} makes a sound`);
+  
+  class Cat extends Animal {
+    purr() {
+          console.log("purr")
+    }
   }
-}
-
-class Dog extends Animal {
-  purr() {
-    console.log("purr");
-  }
-}
-
-class Cat extends Animal {
-  bark() {
-		console.log("woof")
-  }
-}
 
 const animal = new Animal("Unknown");
 console.log(animal.species); 
