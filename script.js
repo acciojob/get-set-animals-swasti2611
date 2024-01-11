ff817ca9aec05b145f3d7fd6af1aeee39e518303
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-11 11:47:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,6 +23,19 @@ class Cat extends Animal {
   }
 }
 
+const animal = new Animal("Unknown");
+console.log(animal.species); 
+animal.makeSound(); 
+
+const cat = new Cat("Cat");
+console.log(cat.species); 
+cat.makeSound(); 
+cat.purr();
+
+const dog = new Dog("Dog");
+console.log(dog.species); 
+dog.makeSound(); 
+dog.bark(); 
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
